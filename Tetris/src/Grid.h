c36145675ca6ec55a57fd8e19ebbb3db9391d81a
szykol <szymonkolton@gmail.com>
2019-06-25 10:22:15 +0200

    Add Grid class

---
+++ Tetris/src/Grid.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+
+#include <SFML/Graphics.hpp>
+
+#include "Cell.h"
+
+class Grid
+{
+    sf::Vector2u m_size;
+    std::vector<Cell> m_grid;
+    const sf::Vector2f m_topLeft;
+public:
+    Grid(const sf::Vector2u& size, const sf::Vector2f& topLeft);
+    void render(sf::RenderTarget &target);
+};
