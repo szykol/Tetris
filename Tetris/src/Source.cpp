7390776e6f9eb77696c48f346d5d6d550e7ad09b
szykol <szymonkolton@gmail.com>
2019-06-25 10:23:09 +0200

    Update Source.cpp

---
+++ Tetris/src/Source.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+#include "Application.h"
+#include "Managers/StateManager.h"
+#include "SimpleMenu.h"
+#include "Managers/CacheSystem.h"
+
+#include <memory>
+
+#include "MainGame.h"
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1200, 800), "Tetris");
+	window.setFramerateLimit(0U);
+
+	Application::init(&window);
+	auto backgroundImage = sen::CacheSystem::get<sf::Texture>("../res/Images/bg.jpeg");
+	Application::setBackgroundImage(*backgroundImage);
+
+	auto temp = std::make_unique<MainGame>();
+	sen::StateManager::pushState(std::move(temp));
+	Application::run();
+
+    return 0;	
+}
