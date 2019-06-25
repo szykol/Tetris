4937c1d240f27f4eed8420a9535ed41b2c548ae8
szykol <szymonkolton@gmail.com>
2019-06-25 10:21:46 +0200

    Add Cell class

---
+++ Tetris/src/Cell.cpp
@@ -0,0 +1,30 @@
+#include "Cell.h"
+
+#include "MainGame.h"
+
+Cell::Cell(const sf::Vector2u& index)
+    : m_gridIndex(index)
+{
+    setSize({CELLSIZE, CELLSIZE});
+    setOutlineThickness(-0.5);
+    setOutlineColor(sf::Color(128,128,128));
+    setFillColor(sf::Color::Black);
+    resetPosition();
+}
+
+void Cell::resetPosition()
+{
+    const auto pos = sf::Vector2f(m_gridIndex.x * CELLSIZE, m_gridIndex.y * CELLSIZE);
+    setPosition(pos + MainGame::getGridTopLeft());
+}
+
+void Cell::render(sf::RenderTarget& target) 
+{
+    target.draw(*this);
+}
+
+void Cell::setIndex(const sf::Vector2i& index) 
+{
+    m_gridIndex = index;
+    resetPosition();
+}
---
+++ Tetris/src/Cell.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+#define CELLSIZE 40
+
+class Cell : public sf::RectangleShape
+{
+    sf::Vector2i m_gridIndex;
+public:
+    Cell(const sf::Vector2u &index);
+    void render(sf::RenderTarget& target);
+    void setIndex(const sf::Vector2i& index);
+    inline const sf::Vector2i getIndex() const { return m_gridIndex; }
+private:
+    void resetPosition();
+};
